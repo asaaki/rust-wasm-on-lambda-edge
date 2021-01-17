7bde5d3877cb818d41372f0b44222f1d42cd51b8
Christoph Grabo <asaaki@mannaz.cc>
2021-01-17 22:48:27 +0100

    Use a more exhaustive call example

--- fixtures/call-test.js
+++ fixtures/call-test.js
@@ -1,30 +1,14 @@
 const originRequest = require("../build.function/origin-request");
-// const otherTrigger = require("../function/REPLACE-ME");
-const sampleEvent = require("./origin-request.json");
+const sampleEvent = require("./origin-request-with-body.json");
 
-// mostly useful for logging and tracing purposes
-const sampleContext = {
-  callbackWaitsForEmptyEventLoop: "[Getter / Setter]",
-  succeed: "[Function]",
-  fail: "[Function]",
-  done: "[Function]",
-  functionVersion: "2",
-  functionName: "edge-node-wasm-test",
-  memoryLimitInMB: "128",
-  logGroupName: "/aws/lambda/edge-node-wasm-test",
-  logStreamName: "2021/01/16/[2]d3b48f1b5fbe4b9690468dce768c20b5",
-  clientContext: undefined,
-  identity: undefined,
-  invokedFunctionArn:
-    "arn:aws:lambda:us-east-1:258911450629:function:edge-node-wasm-test:2",
-  awsRequestId: "9bdfd183-43f8-45dd-baf7-26f0e1511bda",
-  getRemainingTimeInMillis: "[(Function: getRemainingTimeInMillis)]",
-};
+// mostly useful for logging and tracing purposes;
+// note: the actual context is an object with references to functions
+const sampleContext = require("./example-context.json");
+
 // if you need more self-introspection, also consult process.*, but be careful what you expose,
 // for example `process.env` might include sensitive data
 
 (async () => {
-  console.log(
-    global.r = await originRequest.handler(sampleEvent, sampleContext)
-  );
+  const result = await originRequest.handler(sampleEvent, sampleContext);
+  console.log(JSON.stringify(result, undefined, 2));
 })();
